bd6d91534dfb1c06cc941a6b65cd99088afaa2a8
gaurav <gksingh1106@gmail.com>
2026-06-29 10:34:57 +0530

    feat: add reset buttton for grid size

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const divContainer = document.querySelector(".grid-container");
 
-const gridSize = 16;
-
-const  totalGridSize = gridSize * gridSize;
+const resetGrid = document.querySelector(".reset-grid");
 
 let gridBackgroundColor ="#00ADB5";
 
-for(let i = 0; i < totalGridSize; i++){
-    const grid = document.createElement("div");
+createGrid(16)
 
-    grid.classList.add("grid-box")
+function createGrid(gridSize){
 
-    grid.addEventListener("mouseenter", () => {
-        grid.style.backgroundColor = gridBackgroundColor;
-    });
+    const  totalGridSize = gridSize * gridSize;
 
-    divContainer.appendChild(grid);
+    for(let i = 0; i < totalGridSize; i++){
+        const grid = document.createElement("div");
+
+        grid.style.width = `calc(100% / ${gridSize})`;
+        grid.style.height = `calc(100% / ${gridSize})`;
+
+        grid.classList.add("grid-box");
+
+        grid.addEventListener("mouseenter", () => {
+            grid.style.backgroundColor = gridBackgroundColor;
+        });
+
+        divContainer.appendChild(grid);
+    }
 }
+
+resetGrid.addEventListener("click", () => {
+    let newSize = getGridValue();
+    createGrid(newSize);
+});
+
+function getGridValue(){
+    divContainer.innerHTML = "";
+
+    let gridSize = parseInt(prompt("Enter the number of squares per side for the new grid:", 16));
+
+    if (gridSize > 100){
+        gridSize = 100;
+    } else if(gridSize <= 0){
+        gridSize = 16;
+    }
+    return gridSize
+}
+
