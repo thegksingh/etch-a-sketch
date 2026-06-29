0a16780d0af171851e9c601751aeb53de4ef6929
gaurav <gksingh1106@gmail.com>
2026-06-29 09:45:57 +0530

    feat: add hover effect for grid box

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const gridSize = 16;
 
 const  totalGridSize = gridSize * gridSize;
 
+let gridBackgroundColor ="#00ADB5";
+
 for(let i = 0; i < totalGridSize; i++){
     const grid = document.createElement("div");
 
     grid.classList.add("grid-box")
+
+    grid.addEventListener("mouseenter", () => {
+        grid.style.backgroundColor = gridBackgroundColor;
+    });
+
     divContainer.appendChild(grid);
 }
-
